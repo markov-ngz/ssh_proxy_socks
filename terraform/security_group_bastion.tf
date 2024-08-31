5ae278ffd7717eb6cf7dd2b7b37aeec3023e3d95
markov-ngz <batuque77mlh@gmail.com>
2024-08-31 18:59:33 +0200

    ADD : sec rule for ssh jump

--- terraform/security_group_bastion.tf
+++ terraform/security_group_bastion.tf
@@ -3,7 +3,9 @@ resource "aws_security_group" "bastion" {
   description = "Security Group for bastion"
   vpc_id      = aws_vpc.main.id
 
-  tags = var.tags
+  tags = {
+    Name = "ssh_jump_bastion"
+  }
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh_dev" {
@@ -22,6 +24,27 @@ resource "aws_vpc_security_group_egress_rule" "allow_ssh_dev" {
   security_group_id = aws_security_group.bastion.id
 
   cidr_ipv4   = var.public_ip
+  from_port   = 22
+  ip_protocol = "tcp"
+  to_port     = 22
+}
+
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_private" {
+
+  security_group_id = aws_security_group.bastion.id
+  cidr_ipv4         = "10.0.2.0/24"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+  description       = "Allow everyone to  establish an SSH connection"
+  tags              = var.tags
+}
+
+
+resource "aws_vpc_security_group_egress_rule" "allow_ssh_private" {
+  security_group_id = aws_security_group.bastion.id
+  cidr_ipv4   = "10.0.2.0/24"
   from_port   = 22
   ip_protocol = "tcp"
   to_port     = 22
--- terraform/security_group_private.tf
+++ terraform/security_group_private.tf
@@ -3,13 +3,15 @@ resource "aws_security_group" "private" {
   description = "Security Group for private"
   vpc_id      = aws_vpc.main.id
 
-  tags = var.tags
+  tags ={
+    Name = "ssh_jump_private"
+  }
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh_bastion" {
 
   security_group_id = aws_security_group.private.id
-  cidr_ipv4         = aws_instance.bastion.private_ip
+  cidr_ipv4         = "${aws_instance.bastion.private_ip}/32"
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
@@ -21,7 +23,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh_bastion" {
 resource "aws_vpc_security_group_egress_rule" "allow_ssh_bastion" {
   security_group_id = aws_security_group.private.id
 
-  cidr_ipv4   = aws_instance.bastion.private_ip
+  cidr_ipv4   = "${aws_instance.bastion.private_ip}/32"
   from_port   = 22
   ip_protocol = "tcp"
   to_port     = 22
